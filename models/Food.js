459a119d20bc4fc2db1710a2474f7040840b25d1
maurya mitesh <mitesh96625@gmail.com>
2023-02-15 23:18:14 +0530

    moner change in food api

--- models/Food.js
+++ models/Food.js
@@ -2,7 +2,7 @@ const mongo = require('mongoose');
 
 
 const foodModel = mongo.model('food',new mongo.Schema({
-    food:{type:String,required:true,minlength:3,index: {unique: true, dropDups: true}},
+    food:{type:String,required:true,minlength:3,unique:true},
     discription:{type:String,required:true},
     price:{type:Number,required:true},
     foodImage:{type:String,required:true},
--- models/menu.js
+++ models/menu.js
@@ -3,7 +3,6 @@ const mongo = require('mongoose')
 
 const menuModel = mongo.model('menu', new mongo.Schema({
     name: {type:String,required:[true,'must provide menu title'],minlength:3,unique:true},
-    catagory:{type:String,required:true},
     startDate:{type:Date},
     endDate:{type:Date},
     discription: {type:String,minlength:[10,'must be more than 5 words']},
